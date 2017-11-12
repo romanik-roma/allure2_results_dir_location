19468639ab74dfe7318eb3e142bea78883b0ac45
Roma Romanik <rromanik@qartrock.com>
2017-11-12 17:41:55 +0200

    allure.properties in resources

--- src/test/java/com/qa/DummyTest.java
+++ src/test/java/com/qa/DummyTest.java
@@ -2,10 +2,16 @@ package com.qa;
 
 import org.testng.annotations.Test;
 
+import io.qameta.allure.Issue;
+import io.qameta.allure.Link;
+import io.qameta.allure.TmsLink;
+
 public class DummyTest {
 
+	@Link("l-11")
+	@TmsLink("tms-22")
+	@Issue("2233")
 	@Test
 	public void testAllureReportFolderLocation() {
-
 	}
 }
