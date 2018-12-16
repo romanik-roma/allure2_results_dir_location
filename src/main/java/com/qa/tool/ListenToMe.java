0cf6ebb68a022221164b4238914feaf7d55ca483
Roma Romanik <romanikroma@gmail.com>
2018-12-17 00:23:36 +0200

    updated to allure 2.8.1

---
+++ src/main/java/com/qa/tool/ListenToMe.java
@@ -0,0 +1,76 @@
+package com.qa.tool;
+
+import org.testng.ITestContext;
+import org.testng.ITestResult;
+import org.testng.internal.IResultListener2;
+
+public class ListenToMe implements IResultListener2{
+
+	@Override
+	public void onTestStart(ITestResult result) {
+		System.out.println("ON START");
+		
+	}
+
+	@Override
+	public void onTestSuccess(ITestResult result) {
+		System.out.println("ON SUCCESS");
+	}
+
+	@Override
+	public void onTestFailure(ITestResult result) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onTestSkipped(ITestResult result) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onTestFailedButWithinSuccessPercentage(ITestResult result) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onStart(ITestContext context) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onFinish(ITestContext context) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onConfigurationSuccess(ITestResult itr) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onConfigurationFailure(ITestResult itr) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onConfigurationSkip(ITestResult itr) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void beforeConfiguration(ITestResult tr) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+
+}
--- src/test/java/com/qa/DummyTest.java
+++ src/test/java/com/qa/DummyTest.java
@@ -2,16 +2,32 @@ package com.qa;
 
 import org.testng.annotations.Test;
 
+import io.qameta.allure.Attachment;
+import io.qameta.allure.Epic;
+import io.qameta.allure.Feature;
 import io.qameta.allure.Issue;
 import io.qameta.allure.Link;
+import io.qameta.allure.Step;
+import io.qameta.allure.Story;
 import io.qameta.allure.TmsLink;
 
+@Epic("Exploratory")
+@Feature("Dummy")
+@Story("Funny")
 public class DummyTest {
 
-	@Link("l-11")
+	@Link(name = "l-11", type = "my")
 	@TmsLink("tms-22")
 	@Issue("2233")
 	@Test
 	public void testAllureReportFolderLocation() {
+		step1("Make it");
+	}
+	
+	@Step("Do step 1: {0}")
+	@Attachment("Greetings")
+	@io.qameta.allure.Description("X3")
+	private String step1(String action) {
+		return "Hello Allure!";
 	}
 }
